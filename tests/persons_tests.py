4fc8d75989853e8b4166b8520771e7aee3098457
Andela Simulatons <abdulmumeen.olasode@andela.com>
2017-02-08 23:07:16 +0100

    Complete unittests of task 0

--- tests/persons_tests.py
+++ tests/persons_tests.py
@@ -4,13 +4,17 @@ from classes.persons import Persons
 class TestPersons(TestCase):
     def test_add_person_successfully(self):
         new_person = Persons()
-        added = new_person.add_person("Andy Carroll", "STAFF")
+        added = new_person.add_person("Andy Carroll", "staff")
         self.assertTrue(added)
         self.assertEqual(new_person.name,"Andy Carroll")
         second_person = Persons()
-        added = second_person.add_person("Jeremy Johnson", "FELLOW",True)
+        added = second_person.add_person("Jeremy Johnson", "fellow",True)
         self.assertTrue(added)
         self.assertEqual(second_person.name,"Jeremy Johnson")
         self.assertTrue(second_person.livingspace)
 
-    def test_add_person_invalid(self):
+    def test_add_person_invalid_input(self):
+        new_person = Persons()
+        added = new_person.add_person("Andy Carroll", "staff", True)
+        self.assertEqual(added,"Staff cannot request for a livingspace!")
+        self.assertEqual(new_person.name,"")
